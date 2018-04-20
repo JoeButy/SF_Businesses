7ad7aa9a559c8f5b2d920a9df9564139aa8e0b82
Joe Buty <joebuty@gmail.com>
2018-04-19 23:17:26 -0700

    Remove Duplicates
    Drop duplicates on Index once shards compiled.

--- compile_shards_SF_biz.py
+++ compile_shards_SF_biz.py
@@ -1,4 +1,18 @@
+import time
+import pandas as pd #pandas (0.22.0)
+import numpy as np #numpy (1.14.1)
+import datetime as dt
+import re
+from os import listdir
+from os.path import isfile, join
+# Set ipython's max row display to
+pd.set_option('display.max_row', 6)
+# Set iPython's max column width to:
+pd.set_option('display.max_columns', 6)
 
+mypath = r'/users/joebuty/GitHub/SF_Businesses/'
+shard_file_path = r'/users/joebuty/GitHub/SF_Businesses/shards/'
+df = pd.read_csv(mypath + 'df_sf_biz.csv')
 
 ##############################
 #######	COMPILE SHARDS #######
@@ -15,8 +29,8 @@ print final_loc_df.shape
 final_loc_df.columns = ['index', 'Street Location', 'coordinates']
 final_loc_df = final_loc_df.set_index('index', drop=True)
 final_loc_df = final_loc_df.sort_index()
-print list(final_loc_df)
-print final_loc_df
+# print list(final_loc_df)
+# print final_loc_df
 print '\n' *2 
 print 'Location coordinates found.'
 
@@ -36,6 +50,8 @@ df_loc = df_loc[['DBA Name',
 	'Street Location',
 	'coordinates']]
 
+df_loc = df_loc[~df_loc.index.duplicated(keep='first')]
 df_loc.to_csv(mypath + 'formatted_sf_biz.csv')
 
 print df_loc
+
